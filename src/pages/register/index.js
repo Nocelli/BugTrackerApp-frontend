24fda81b741d2ea9e0c75b853689f13ead7f5886
Nocelli <rafaelnocelliifsp@gmail.com>
2020-04-10 01:50:56 -0300

    added api request to registration page

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import './style.css'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
-import NavBar from '../../components/NavBar/NavBar'
 import api from '../../services/api'
 import ErrorRenderer from '../../components/ErrorRenderer/ErrorRenderer'
 import * as yup from 'yup'
 
+import { ReactComponent as Background } from '../../assets/signup_background.svg'
+import { ReactComponent as Avatar } from '../../assets/avatar.svg'
+
 const Register = () => {
+
+    const [errors, setErrors] = useState(null)
+    const history = useHistory()
 
     const validationSchema = yup.object({
         email: yup.string().email().required(),
@@ -14,29 +20,50 @@ const Register = () => {
         password: yup.string().required().min(8).max(16)
     })
 
+    async function handleSubmitting({ name, email, password, confirmPass }) {
+        try {
+            setErrors(null)
+            if (password != confirmPass)
+                setErrors('Por favor, confirme sua senha antes de prosseguir')
+            const response = await api.post('users', { name, email, password })
+            if (response)
+                history.push('/confirmations')
+        }
+        catch (err) {
+            const { response } = err;
+            setErrors(response ? response.data.message || response.data.error : 'Não foi possível estabelecer uma conexão com o servidor')
+        }
+    }
+
     return (
         <div className='form-container'>
-            <NavBar />
-            <Formik initialValues={{ email: '',name:'',password:'' }}
+            <Background className='background' />
+            <ErrorRenderer errors={errors} />
+            <Formik initialValues={{ email: '', name: '', password: '', confirmPass: '' }}
                 onSubmit={async (data, { setSubmitting }) => {
                     setSubmitting(true)
-                    
-                        const response = await api.post('users', {name: data.name , email: data.email, password: data.password})
-                        console.log(response);
-                        
-                    
+                    await handleSubmitting(data)
                     setSubmitting(false)
                 }}
                 validationSchema={validationSchema}>
-                {({ values, isSubmitting, handleSubmit, handleChange, handleBlur }) => (
+                {({ isSubmitting }) => (
                     <Form className='form'>
+                        <Avatar className='signup-avatar' />
+                        <h1>Cadastre-se</h1>
+                        <h3>crie uma conta rápido e fácil</h3>
                         <Field name='name' placeholder='Nome' type='input' />
                         <ErrorMessage render={msg => <ErrorRenderer errors={msg} />} name="name" />
                         <Field name='email' placeholder='E-mail' type='email' />
                         <ErrorMessage render={msg => <ErrorRenderer errors={msg} />} name="email" />
                         <Field name='password' placeholder='Senha' type='password' />
                         <ErrorMessage render={msg => <ErrorRenderer errors={msg} />} name="password" />
-                        <button type='submit' disabled={isSubmitting}>Cadastrar</button>
+                        <Field name='confirmPass' placeholder='Confirme a Senha' type='password' />
+                        <button type='submit' disabled={isSubmitting}>Criar conta</button>
+                        <div className='links-holder'>
+                            <span>Já tem uma conta? </span>
+                            <Link className='link' to="/login">
+                                Entrar</Link>
+                        </div>
                     </Form>
                 )}
             </Formik>
